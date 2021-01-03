734b630a2cb76023e64ba6d6622abe8a641ff6e5
Farkhad Muminov <libreno@gmail.com>
2021-01-03 14:22:27 +0400

    Kadane's algorythm based solution, Correct Answer.

--- 00400-flip/program.js
+++ 00400-flip/program.js
@@ -1,47 +1,34 @@
 function flip(A) {
     let S = Array.from(A, x => Number(x));
-    let beginIndexes = [];
-    let endIndexes = [];
-    let was1 = true;
-    for(let i = 0; i < S.length; i++){
-        if (S[i] === 0 && was1){
-            beginIndexes.push(i);
-            was1 = false;
-            continue;
-        };
-        if (S[i] === 1 && !was1){
-            endIndexes.push(i);
-            was1 = true;
-        };
-    };
-    if (beginIndexes.length === 0){
-        return [];
-    };
-    if (!was1){
-        endIndexes.push(S.length);
-    };
     let maxWeight = 0;
-    let maxWeightI = 0;
-    let maxWeightJ = 0;
-    for (let i = 0; i < beginIndexes.length; i++){
-        for (let j = i; j < endIndexes.length; j++){
-            let weight = 0;
-            for(let k = beginIndexes[i]; k < endIndexes[j]; k++){
-                weight += S[k] === 0? 1 : -1;
-            };
-            // console.log(beginIndexes[i] + ':' + (endIndexes[j] - 1) + '=[' + S.slice(beginIndexes[i], endIndexes[j]) + '], weight=' + weight);
+    let maxWeightL = maxWeightR = -1;
+    let weight = 0;
+    let l = r = 0;
+    for (let i = 0; i < S.length; i++){
+        if (S[i] === 0) {
+            weight++;
             if (weight > maxWeight){
                 maxWeight = weight;
-                maxWeightI = beginIndexes[i]+1;
-                maxWeightJ = endIndexes[j];
-            }
+                maxWeightL = l;
+                maxWeightR = i;
+            };
+        } else {
+            weight--;
+            if (weight < 0){
+                weight = 0;
+                l = i + 1;
+                r = i + 1;
+            };
         };
     };
-    return [maxWeightI, maxWeightJ];
+    if (maxWeightR === -1){
+        return [];
+    };
+    return [maxWeightL + 1, maxWeightR + 1];
 }
 
 // console.log(flip([1,0,1,0,0,0,0,0,1,1,1,0,1,0,0,0,0,1,1,0]));
 // console.log(flip('1101010001'));
-// console.log(flip('010'));
-console.log(flip('1111'));
+console.log(flip('010'));
+// console.log(flip('1111'));
 console.log(flip('0'));
